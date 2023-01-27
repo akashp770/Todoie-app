9b3cbd6c58f3952f71b7692bc3566fe05b4d8307
Akash <akashp77055@gmal.com>
2023-01-27 20:11:07 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "./components/Header.jsx";
+import TodoItem from "./components/TodoItems";
+import Button from "./components/Button.jsx";
+
+import "./style.css";
 
 const App = () => {
-  return <p>Todoie App</p>;
+  return (
+    <div className="todo-container">
+      <Header />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <Button />
+    </div>
+  );
 };
 
 export default App;
